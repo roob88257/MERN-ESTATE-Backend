6dd77f1c1183d9c80866d6883c462f8cb0adbf59
roob88257 <traj88257@gmail.com>
2023-12-31 03:07:03 +0530

    Signin API completed successfully

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs"
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup=async(req, res, next) => {
    const {username,email,password} = req.body;
@@ -18,3 +19,41 @@ export const signup=async(req, res, next) => {
    // console.log(req.body);
 
   };
+
+
+  export const signin =async(req, res, next) => {
+
+    const {email,password}=req.body;
+
+    try {
+
+    
+    const verifiedUser= await User.findOne({email: email});
+
+    if(!verifiedUser)
+    {
+        return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword= bcryptjs.compareSync(password,verifiedUser.password);
+
+    if(!validPassword)
+    {
+        return next(errorHandler(401, "Wrong Credentials"));
+    }
+    
+    const token= jwt.sign({id: verifiedUser._id},process.env.JWT_SECRET); 
+
+    // console.log(verifiedUser);
+
+    const {password:pass, ...rest}=verifiedUser._doc;
+
+    res.cookie('jwt', token, { httpOnly: true });
+    res.status(200);
+    res.json(rest);
+}
+catch (err) {
+    next(err);
+}
+
+  }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cors from 'cors';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -14,6 +15,8 @@ console.log(err);
 
 const app = express();
 
+app.use(cors());
+
 app.listen(5000, () => {
     console.log(`Server is running at port 5000 !!!!`);
    
@@ -22,6 +25,7 @@ app.listen(5000, () => {
   app.use(express.json());
   app.use('/api/user', userRouter);
   app.use('/api/auth', authRouter);
+ 
 
   app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
