addf02e8af650d74c626087352f01476bb6793ba
roob88257 <traj88257@gmail.com>
2024-01-20 19:55:08 +0530

    Google OAuth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -50,12 +50,57 @@ export const signup=async(req, res, next) => {
 
 
     // console.log(token); 
+    console.log(rest);
     res.cookie('acess_token', token, { httpOnly: true });
     res.status(200);
     res.json(rest);
 }
 catch (err) {
-    next(err);
+    console.log(err);
 }
+
+  }
+
+
+  export const google=async (req,res,next)=>{
+    
+    const {email,username,image} = req.body;
+    try {
+         
+        const user= await User.findOne({email: email});
+
+        if(user)
+        {
+              const token= jwt.sign({id: username._id}, process.env.JWT_SECRET);
+              const {password:pass, ...rest}=user._doc;
+              res.cookie('access_token', token, {httpOnly:true})
+              res.status(200);
+              res.json(rest);
+        }
+        else
+        {
+         
+            const generatedPassword= Math.random().toString(36).slice(-8)+Math.random().toString(36).slice(-8);
+        const hashedPassword=bcryptjs.hashSync(generatedPassword,10);
+        const newUser=new User({username:username.split(" ").join("").toLowerCase()+Math.random().toString(36).slice(-8),email,password:hashedPassword,avatar:image});
+
+        await newUser.save();
+
+         const token=jwt.sign({id:newUser._id}, process.env.JWT_SECRET);
+         const {password:pass, ...rest}= newUser._doc;
+          
+         res.cookie('access_token', token, {httpOnly:true})
+              res.status(200);
+              res.json(rest);
+        }
+
+ 
+
+
+      }
+      catch (err){
+       next(err);
+      }
+       
 
   }
--- models/user.model.js
+++ models/user.model.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    avatar: {type:String, default:"https://imgs.search.brave.com/mDdtZ12xiGTjupVmFywXaqmw7taeD-L12YCXsD02hPQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAxLzA3LzQzLzQ1/LzM2MF9GXzEwNzQz/NDUxMV9pYXJGMno4/OGM2RHM2QWxndHdv/dEhTQWt0V0NkWU9u/Ny5qcGc" },
     
     // Add more fields as needed for a real estate website
   },
